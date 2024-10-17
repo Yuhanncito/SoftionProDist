854ba5f7613a0b70c63b00094cf891e275deb7ca
Yuhanncito <uchijaisuka02@gmail.com>
2024-10-16 22:58:12 -0600

    Fix Service Worked Push Notifications

--- registerSW.js
+++ registerSW.js
@@ -1,7 +1,7 @@
 const PUBLICKEY = "BBeSyh4-JimNpNCTwdyyUBujq1iaIoOby-Kqk8yv_GE7JAjOf8LU8xJVT9poP3Ccch0Q8IugJsGQVUnoojAxSXo";
 
-    const Url2 = 'http://localhost:4000/api/test';
-  const Url = 'https://softion-api-v3.vercel.app/api/test';
+    const Url = 'http://localhost:4000/api/test';
+  const Url2 = 'https://softion-api-v3.vercel.app/api/test';
 
 function urlBase64ToUint8Array(base64String) {
   const padding = "=".repeat((4 - (base64String.length % 4)) % 4);
@@ -17,27 +17,52 @@ function urlBase64ToUint8Array(base64String) {
 
 
 if ("serviceWorker" in navigator && "PushManager" in window) {
-  window.addEventListener("load", () => {
-    navigator.serviceWorker.register("/sw.js", { scope: "/" })
-    .then((registration) => {
-        return registration.pushManager.subscribe({
-          userVisibleOnly: true,
-          applicationServerKey: urlBase64ToUint8Array(PUBLICKEY),
+  window.addEventListener("load", async () => {
+    try {
+      const registration = await navigator.serviceWorker.register("/sw.js", { scope: "/" });
+      let activeWorker;
+
+      if (registration.waiting) {
+        activeWorker = registration.waiting;
+      } else if (registration.installing) {
+        activeWorker = await new Promise((resolve, reject) => {
+          registration.installing.addEventListener("statechange", (e) => {
+            if (registration.installing.state === "activated") {
+              resolve(registration.installing);
+            } else if (registration.installing.state === "redundant") {
+              reject(new Error("Installation failed"));
+            }
+          });
         });
-      })
-      .then((subscription) => {
-        return fetch(Url, {
+      } else {
+        activeWorker = registration.active;
+      }
+
+      if (activeWorker) {
+        // Verifica si ya existe una suscripción
+        let subscription = await registration.pushManager.getSubscription();
+        if (!subscription) {
+          subscription = await registration.pushManager.subscribe({
+            userVisibleOnly: true,
+            applicationServerKey: urlBase64ToUint8Array(PUBLICKEY),
+          });
+        }
+
+        await fetch(Url, {
           method: "POST",
           body: JSON.stringify(subscription),
           headers: {
             "Content-Type": "application/json",
           },
-        }).then( ()=>{
-          window.localStorage.setItem('subscription', JSON.stringify(subscription));
-        } );
-      })
-      .catch((error) =>
-        console.error("Error during service worker registration:", error)
-      );
+        });
+
+        window.localStorage.setItem('subscription', JSON.stringify(subscription));
+      }
+    } catch (error) {
+      console.error("Error durante el registro del Service Worker:", error);
+    }
   });
 }
+
+
+
--- sw.js
+++ sw.js
@@ -1,4 +1,89 @@
-if(!self.define){let e,i={};const s=(s,f)=>(s=new URL(s+".js",f).href,i[s]||new Promise((i=>{if("document"in self){const e=document.createElement("script");e.src=s,e.onload=i,document.head.appendChild(e)}else e=s,importScripts(s),i()})).then((()=>{let e=i[s];if(!e)throw new Error(`Module ${s} didn’t register its module`);return e})));self.define=(f,r)=>{const n=e||("document"in self?document.currentScript.src:"")||location.href;if(i[n])return;let o={};const l=e=>s(e,n),t={module:{uri:n},exports:o,require:l};i[n]=Promise.all(f.map((e=>t[e]||l(e)))).then((e=>(r(...e),o)))}}define(["./workbox-7cfec069"],(function(e){"use strict";self.addEventListener("message",(e=>{e.data&&"SKIP_WAITING"===e.data.type&&self.skipWaiting()})),e.precacheAndRoute([{url:"assets/index-B_S6gtpN.css",revision:null},{url:"assets/index-DkqenzKT.js",revision:null},{url:"index.html",revision:"f6b74cc5a73bab2538ac6d7ec89afff2"},{url:"registerSW.js",revision:"bffaacfd7a0766dc14efff8f56cd9854"},{url:"service-worker.js",revision:"e9104e72783ba8667fa411ad5ac165b8"},{url:"logo.png",revision:"4f5f6a22be3fa16afeec064ee57adcb5"},{url:"images/logo.png",revision:"4f5f6a22be3fa16afeec064ee57adcb5"},{url:"images/logo_192x192.png",revision:"2aab3b1fff6535f6faf6c91ac83bfedd"},{url:"images/logo_512x512.png",revision:"4c3776fe8d98bf6479315bf68e661ad5"},{url:"manifest.webmanifest",revision:"3b27294350b8769528dbef960e524f68"}],{}),e.cleanupOutdatedCaches(),e.registerRoute(new e.NavigationRoute(e.createHandlerBoundToURL("index.html")))}));
+if (!self.define) {
+  let e,
+    s = {};
+  const i = (i, n) => (
+    (i = new URL(i + ".js", n).href),
+    s[i] ||
+      new Promise((s) => {
+        if ("document" in self) {
+          const e = document.createElement("script");
+          (e.src = i), (e.onload = s), document.head.appendChild(e);
+        } else (e = i), importScripts(i), s();
+      }).then(() => {
+        let e = s[i];
+        if (!e) throw new Error(`Module ${i} didn’t register its module`);
+        return e;
+      })
+  );
+  self.define = (n, r) => {
+    const t =
+      e ||
+      ("document" in self ? document.currentScript.src : "") ||
+      location.href;
+    if (s[t]) return;
+    let f = {};
+    const a = (e) => i(e, t),
+      c = { module: { uri: t }, exports: f, require: a };
+    s[t] = Promise.all(n.map((e) => c[e] || a(e))).then((e) => (r(...e), f));
+  };
+}
+define(["./workbox-7cfec069"], function (e) {
+  "use strict";
+  self.addEventListener("message", (e) => {
+    e.data && "SKIP_WAITING" === e.data.type && self.skipWaiting();
+  }),
+    e.precacheAndRoute(
+      [
+        { url: "assets/index-CCe06EyD.js", revision: null },
+        { url: "assets/index-CR4B3lXP.css", revision: null },
+        {
+          url: "ganttStyles/frappe-gantt.css",
+          revision: "c6ebdf349f99b3c0db6c093e33bc723f",
+        },
+        {
+          url: "ganttStyles/frappe-gantt.js",
+          revision: "ab82e6e4003ba93b6bec487f89adb289",
+        },
+        {
+          url: "ganttStyles/frappe-gantt.min.css",
+          revision: "fef88de645caec679ec2c8602388edcd",
+        },
+        {
+          url: "ganttStyles/frappe-gantt.min.js",
+          revision: "e131076fc0acdc62d81f1c5e8eec7b71",
+        },
+        { url: "index.html", revision: "3db5228f334e62ea645d6049a4e1cf46" },
+        { url: "registerSW.js", revision: "54b93b4d2874635550a6cd9b92cf9d3b" },
+        {
+          url: "service-worker.js",
+          revision: "e9104e72783ba8667fa411ad5ac165b8",
+        },
+        { url: "logo.png", revision: "4f5f6a22be3fa16afeec064ee57adcb5" },
+        {
+          url: "images/logo.png",
+          revision: "4f5f6a22be3fa16afeec064ee57adcb5",
+        },
+        {
+          url: "images/logo_192x192.png",
+          revision: "2aab3b1fff6535f6faf6c91ac83bfedd",
+        },
+        {
+          url: "images/logo_512x512.png",
+          revision: "4c3776fe8d98bf6479315bf68e661ad5",
+        },
+        {
+          url: "manifest.webmanifest",
+          revision: "3b27294350b8769528dbef960e524f68",
+        },
+      ],
+      {}
+    ),
+    e.cleanupOutdatedCaches(),
+    e.registerRoute(
+      new e.NavigationRoute(e.createHandlerBoundToURL("index.html"))
+    );
+});
+//# sourceMappingURL=sw.js.map
 self.addEventListener('push', event => {
     const data = event.data.json();
     self.registration.showNotification(data.title, {
