c5fb901b1ef722647f5106ef6f05b80e1a7d84ca
Yuhanncito <uchijaisuka02@gmail.com>
2024-11-08 03:28:02 -0600

    cam and cache

--- registerSW.js
+++ registerSW.js
@@ -1,7 +1,7 @@
 const PUBLICKEY = "BBeSyh4-JimNpNCTwdyyUBujq1iaIoOby-Kqk8yv_GE7JAjOf8LU8xJVT9poP3Ccch0Q8IugJsGQVUnoojAxSXo";
 
-    const Url2 = 'http://localhost:4000/api/test';
-  const Url = 'https://softion-api-v3.vercel.app/api/test';
+const Url2 = 'http://localhost:4000/api/test';
+const Url = 'https://softion-api-v3.vercel.app/api/test';
 
 function urlBase64ToUint8Array(base64String) {
   const padding = "=".repeat((4 - (base64String.length % 4)) % 4);
@@ -14,12 +14,21 @@ function urlBase64ToUint8Array(base64String) {
   return outputArray;
 }
 
-
+let isRegistered = window.localStorage.getItem('isRegistered') === 'true';
 
 if ("serviceWorker" in navigator && "PushManager" in window) {
   window.addEventListener("load", async () => {
+    if (isRegistered) {
+      console.log('Service Worker ya registrado');
+      return;
+    }
+
+    
+
     try {
       const registration = await navigator.serviceWorker.register("/sw.js", { scope: "/" });
+      console.log("Service Worker registrado:", registration);
+
       let activeWorker;
 
       if (registration.waiting) {
@@ -43,12 +52,16 @@ if ("serviceWorker" in navigator && "PushManager" in window) {
       }
 
       if (activeWorker) {
+        console.log("Active Worker:", activeWorker);
         let subscription = await registration.pushManager.getSubscription();
+        console.log("Current subscription:", subscription);
+
         if (!subscription) {
           subscription = await registration.pushManager.subscribe({
             userVisibleOnly: true,
             applicationServerKey: urlBase64ToUint8Array(PUBLICKEY),
           });
+          console.log("New subscription:", subscription);
         }
 
         await fetch(Url, {
@@ -60,12 +73,19 @@ if ("serviceWorker" in navigator && "PushManager" in window) {
         });
 
         window.localStorage.setItem('subscription', JSON.stringify(subscription));
+        window.localStorage.setItem('isRegistered', 'true');
+        isRegistered = true;
       }
     } catch (error) {
       console.error("Error durante el registro del Service Worker:", error);
     }
+
+    navigator.serviceWorker.addEventListener('message', event => { 
+      if (event.data && event.data.type === 'SW_ACTIVATED') 
+        { 
+          console.log('Service Worker activado recargado');
+          alert('El Service Worker ha sido activado. Por favor, recarga la página para habilitar las notificaciones push.'); 
+        } 
+      });
   });
 }
-  
-
-
